655fdcf8f0601c32860eaa4a40b33aeed948f9f0
Reise Dawson <reisedawson@gmail.com>
2021-01-25 15:53:16 +0000

    setting up db connection scripts and commenting out SQL scripts that are not yet available for test running create_tables.py

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -50,15 +50,15 @@ time_table_insert = ("""
 """)
 
 # QUERY LISTS
-create_table_queries = [staging_events_table_create,
-                        staging_songs_table_create,
+create_table_queries = [#staging_events_table_create,
+                        #staging_songs_table_create,
                         songplay_table_create,
                         user_table_create,
                         song_table_create,
                         artist_table_create,
                         time_table_create]
-drop_table_queries = [staging_events_table_drop,
-                      staging_songs_table_drop,
+drop_table_queries = [#staging_events_table_drop,
+                      #staging_songs_table_drop,
                       songplay_table_drop,
                       user_table_drop,
                       song_table_drop,
